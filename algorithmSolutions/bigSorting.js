61f21d2ae8942bed9a31e79ad705f607de560f3b
Işınnur Günay <isinngnyy@gmail.com>
2025-02-13 09:47:14 +0300

    feat: add big sorting algorithm solution

---
+++ algorithmSolutions/bigSorting.js
@@ -0,0 +1,9 @@
+function bigSorting(unsorted) {
+  return unsorted.sort((a, b) => {
+    if (a.length !== b.length) {
+      return a.length - b.length;
+    } else {
+      return a.localeCompare(b);
+    }
+  });
+}
