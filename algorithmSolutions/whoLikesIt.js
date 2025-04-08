084bbd13940f14ec6f72fe9c222747167fe1c2fb
Işınnur Günay <isinngnyy@gmail.com>
2025-04-08 16:28:03 +0300

    fix: update case handling

--- algorithmSolutions/whoLikesIt.js
+++ algorithmSolutions/whoLikesIt.js
@@ -10,7 +10,7 @@ function likes(names) {
       return `${names[0]} and ${names[1]} like this`;
     case 3:
       return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-    case 4:
+    default:
       return `${names[0]}, ${names[1]} and ${count - 2} others like this`;
   }
 }
